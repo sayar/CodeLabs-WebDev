755f4c89e1b81726674b40cc3d387a450f7913b1
Rami Sayar <rami.sayar@gmail.com>
2016-10-30 07:52:14 -0400

    Fixed spacing.

--- Module4-FrontEndDev/Source/Ex2/Begin/src/OrderApp/wwwroot/js/app.module.ts
+++ Module4-FrontEndDev/Source/Ex2/Begin/src/OrderApp/wwwroot/js/app.module.ts
@@ -1,8 +1,8 @@
 ﻿import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
-import {AgGridModule} from 'ag-grid-ng2/main';
-import {DetailsGridComponent} from "./details-grid.component";
+import { AgGridModule } from 'ag-grid-ng2/main';
+import { DetailsGridComponent } from "./details-grid.component";
 
 @NgModule({
     imports: [BrowserModule, AgGridModule.withNg2ComponentSupport()],
